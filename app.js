951596dcd1e7d5ee69e493b781f86f7e1720baa1
Abdiox <abdirahmanroble2610@gmail.com>
2023-02-22 22:44:05 +0100

    yes

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
 "use strict";
 
+let points = 0;
+let lives = 3;
+
 window.addEventListener("load", start);
+
+function start() {
+  console.log("Keepy ups!");
+
+  document.querySelector("#football1_container").classList.add("falling");
+  document.querySelector("#small_bombs1_sprite").classList.add("falling");
+  document
+    .querySelector("#football1_container")
+    .addEventListener("click", clickBall);
+  document
+    .querySelector("#small_bombs1_sprite")
+    .addEventListener("click", clickBomb);
+}
